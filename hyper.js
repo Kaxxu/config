0f8899eff2956b4894658855549c41b5ceb28981
Kaxxu <pawmentel@gmail.com>
2019-01-19 16:29:35 +0100

    v1.3

--- hyper.js
+++ hyper.js
@@ -36,7 +36,7 @@ module.exports = {
     cursorShape: 'BLOCK',
 
     // set to `true` (without backticks and without quotes) for blinking cursor
-    cursorBlink: true,
+    cursorBlink: false,
 
     // color of the text
     foregroundColor: '#fff',
@@ -75,9 +75,9 @@ module.exports = {
     colors: {
       black: '#000000',
       red: '#C51E14',
-      green: '#1DC121',
+      green: '#47d147',
       yellow: '#C7C329',
-      blue: '#0a8cc4',
+      blue: '#3399ff',
       magenta: '#C839C5',
       cyan: '#20C5C6',
       white: '#C7C7C7',
@@ -85,9 +85,9 @@ module.exports = {
       lightRed: '#FD6F6B',
       lightGreen: '#67F86F',
       lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
+      lightBlue: '#33ccff',
       lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
+      lightCyan: '#a366ff',
       lightWhite: '#FFFFFF',
     },
 
